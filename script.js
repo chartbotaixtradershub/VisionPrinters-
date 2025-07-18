b77d300d9068399e15fc8747187faa06a101262f
Binary deriv <oderobetty5@gmail.com>
2025-07-19 00:12:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+  setTimeout(() => {
+    document.querySelector('.overlay').style.display = 'none';
+    document.getElementById('dashboard-frame').style.display = 'block';
+  }, 4000);
+};
