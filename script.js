13c7d8fc357f4f5f6bab50f9cf5938edabe85289
Binary deriv <oderobetty5@gmail.com>
2025-07-19 01:57:01 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-  setTimeout(() => {
-    document.querySelector('.overlay').style.display = 'none';
-    document.getElementById('dashboard-frame').style.display = 'block';
-  }, 4000);
-};
